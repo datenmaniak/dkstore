4c84a9cbbd740c22eeb2b1df457800fb35e56d67
datenmaniak <ppwj@yahoo.com>
2025-11-20 13:42:22 -0400

    Added: Dark Mode activated by System setting

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,9 +1,43 @@
 (() => {
-    const darkModeBtn = document.querySelector('.dark-mode-button');
+    document.addEventListener('DOMContentLoaded', function () {
 
-    // Escuchamos el click
-    darkModeBtn.addEventListener('click', () => {
-        darkModeBtn.classList.toggle('active');
-        document.body.classList.toggle('dark-mode');
+        darkMode()
+
+
     });
+
+
+    function darkMode() {
+
+        const preferredDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+        console.log('Current theme: ', preferredDarkMode.matches ? 'dark' : 'light');
+
+        if (preferredDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+
+            document.body.classList.remove('dark-mode');
+
+        }
+
+        preferredDarkMode.addEventListener('change', (event) => {
+            if (event.matches) {
+                document.body.classList.add('dark-mode');
+            } else {
+                document.body.classList.remove('dark-mode');
+            }
+
+        });
+
+        const darkModeBtn = document.querySelector('.dark-mode-button');
+
+        // Escuchamos el click
+        darkModeBtn.addEventListener('click', () => {
+            darkModeBtn.classList.toggle('active');
+            document.body.classList.toggle('dark-mode');
+            console.log('Dark mode active');
+        });
+
+    }
 })();
