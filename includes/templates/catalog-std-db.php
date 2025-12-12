ad3b968ad7411cfd86db1abcc1142de74af24d16
datenmaniak <ppwj@yahoo.com>
2025-12-12 19:38:36 -0400

    Change catalog order: updated_at DESC

--- includes/templates/catalog-std-db.php
+++ includes/templates/catalog-std-db.php
@@ -19,7 +19,7 @@
     $db = conectDB();
 
     // get the records.
-    $query = "SELECT * FROM productos WHERE is_deleted = 0 AND is_active = 1 ORDER BY created_at DESC;";
+    $query = "SELECT * FROM productos WHERE is_deleted = 0 AND is_active = 1 ORDER BY updated_at DESC;";
     // Excluye los que tienen un soft-deleted y aquellos que ha sido marcado
     // para mostrarse en el catálogo.
 
